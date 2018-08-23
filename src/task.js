efbd82e5af442c859101e95e3354450eff541231
chrisisler <christopherisler1@gmail.com>
2018-08-23 00:28:12 -0500

    [wip] progress

--- src/task.js
+++ src/task.js
@@ -1,32 +1,124 @@
+// @flow
+
 import { warning, defer } from './util'
 
 const Pending = 'Pending'
 const Resolved = 'Resolved'
 const Rejected = 'Rejected'
 
-export default function Task(executor) {
+// ecma-international.org/ecma-262/6.0/#sec-promise-objects
+export default function Task(executor: Function) {
   if (!(this instanceof Task)) {
-    throw TypeError('Cannot call the Task class as a function')
+    throw TypeError('Cannot call `Task` as a function, use `new Task` instead')
   } else if (typeof executor !== 'function') {
     throw TypeError('Expected a function')
   } else if (executor.length < 1) {
     throw TypeError('Expected function to take at least one argument')
   }
 
+  let hasCatch = false
   let value = void 0
   let state = Pending
-  let hasCatch = false
+  let resolveReactions = []
+  let rejectReactions = []
 
-  executor(
-    function resolve(resolvedValue) {
-      state = Resolved
-      value = resolvedValue
-    },
-    function reject(rejectedValue) {
-      state = Rejected
-      value = rejectedValue
-    }
-  )
+  {
+    let executed = false
 
-  this.then = (onResolved, onRejected) => {}
+    executor(
+      function resolve(resolution) {
+        if (executed) {
+          return
+        }
+        executed = true
+        state = Resolved
+        value = resolution
+      },
+      function reject(rejection) {
+        if (executed) {
+          return
+        }
+        executed = true
+        state = Rejected
+        value = rejection
+      }
+    )
+  }
+
+  this.__proto__.catch = onRejected => {
+    hasCatch = true
+    return this.then(void 0, onRejected)
+  }
+
+  // ecma-international.org/ecma-262/6.0/#sec-promise.prototype.then
+  this.__proto__.then = (onResolved, onRejected) => {
+    return new Task((resolve, reject) => {
+      if (typeof onResolved !== 'function') {
+        onResolved = function identity(x) {
+          return x
+        }
+      }
+      if (typeof onRejected !== 'function') {
+        onRejected = function thrower(x) {
+          throw x
+        }
+      } else {
+        hasCatch = true
+      }
+
+      let createReaction = handler => value => {
+        try {
+          resolve(handler(value))
+        } catch (error) {
+          reject(error)
+        }
+      }
+
+      let _resolveReaction = createReaction(onResolved)
+      let _rejectReaction = createReaction(onRejected)
+
+      if (state === Pending) {
+        resolveReactions.push(_resolveReaction)
+        rejectReactions.push(_rejectReaction)
+      } else {
+        defer(() => {
+          if (state === Resolved) {
+            _resolveReaction(value)
+          } else if (state === Rejected) {
+            _rejectReaction(value)
+          }
+        })
+      }
+    })
+  }
+
+  // $FlowFixMe
+  this.__proto__[Symbol.toStringTag] = `<${value}>`
 }
+
+Task.resolve = resolution => {
+  if (resolution && typeof resolution.then === 'function') {
+    if (resolution.constructor.name === 'Task') {
+      return resolution
+    }
+    return resolution.then(
+      resolved => {
+        return new Task(resolve => {
+          resolve(resolved)
+        })
+      },
+      rejected => {
+        return new Task((_, reject) => {
+          reject(rejected)
+        })
+      }
+    )
+  }
+  return new Task(resolve => {
+    resolve(resolution)
+  })
+}
+
+// Task.reject = rejection => {}
+// Task.all = tasks => {}
+// Task.race = tasks => {}
